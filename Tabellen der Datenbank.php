90fe81aebc7d45d0a7e4a8cd9640bea8fa5b02f6
Mockse <felix.mocker@outlook.de>
2025-02-26 14:58:51 +0100

    Upload Database in .php

---
+++ Tabellen der Datenbank.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `tierfutter_webshop`
+ */
+
+/* `tierfutter_webshop`.`produkt` */
+$produkt = array(
+  array('id' => 'P001','name' => 'Hundefutter Bio-Rind Trockenfutter','preis' => '22.99','kategorie_id' => '1','tierart' => '1'),
+  array('id' => 'P002','name' => 'Katzenfutter Feine Häppchen Lachs','preis' => '18.49','kategorie_id' => '2','tierart' => '2'),
+  array('id' => 'P003','name' => 'Hundesnack Hühnchenstreifen','preis' => '6.99','kategorie_id' => '3','tierart' => '1'),
+  array('id' => 'P004','name' => 'Katzenfutter Geflügel in Soße','preis' => '12.49','kategorie_id' => '2','tierart' => '2'),
+  array('id' => 'P005','name' => 'Hundefutter Getreidefrei Lamm','preis' => '25.99','kategorie_id' => '2','tierart' => '1'),
+  array('id' => 'P006','name' => 'Katzenfutter Sensitiv Trockenfutter','preis' => '16.99','kategorie_id' => '1','tierart' => '2'),
+  array('id' => 'P007','name' => 'Hundesnack Rinderkaustange','preis' => '8.99','kategorie_id' => '3','tierart' => '1'),
+  array('id' => 'P008','name' => 'Katzensnack Knuspertaschen Käse','preis' => '3.49','kategorie_id' => '3','tierart' => '2'),
+  array('id' => 'P009','name' => 'Hundefutter Senior Huhn & Reis','preis' => '19.99','kategorie_id' => '2','tierart' => '1'),
+  array('id' => 'P010','name' => 'Katzenfutter Kitten Pute','preis' => '14.99','kategorie_id' => '2','tierart' => '2')
+);
+
+/* `tierfutter_webshop`.`produktkategorie` */
+$produktkategorie = array(
+  array('kategorie_id' => '1','kategorie' => 'Trockenfutter'),
+  array('kategorie_id' => '2','kategorie' => 'Nassfutter'),
+  array('kategorie_id' => '3','kategorie' => 'Snacks')
+);
